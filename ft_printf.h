dc7f37ba9d91815d8ed8f879cd61f6bb26a139cd
Pradipas Uttasa <puttasa@z1t1c4.42bangkok.com>
2022-09-05 01:33:27 +0700

    edit header

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 14:43:04 by puttasa           #+#    #+#             */
-/*   Updated: 2022/09/04 16:09:32 by puttasa          ###   ########.fr       */
+/*   Updated: 2022/09/05 01:32:42 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-int		ft_printf(const char *, ...);
+int		ft_printf(const char *str, ...);
 int		ft_check(va_list ptr_arg, const char str);
 int		ft_putchar(char c);
 int		ft_putstr(char *str);
